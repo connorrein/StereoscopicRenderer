f5e7ff75a0947b2ac2c87bd6fd98d974c7b04bb2
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-06-05 19:44:03 -0700

    Camera

---
+++ StereoscopicRenderer/Camera.cpp
@@ -0,0 +1,84 @@
+#include "Camera.h"
+
+Camera::Camera(int width, int height, glm::vec3 position)
+{
+    Camera::width = width;
+    Camera::height = height;
+    Camera::position = position;
+}
+
+void Camera::Matrix(float fovDeg, float nearPlane, float farPlane,
+    Shader& shader, const char* uniform)
+{
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 proj = glm::mat4(1.0f);
+
+    view = glm::lookAt(position, position + forward, up);
+    proj = glm::perspective(glm::radians(fovDeg), (float)width / height, nearPlane, farPlane);
+
+    glUniformMatrix4fv(glGetUniformLocation(shader.Id(), uniform), 1, GL_FALSE,
+        glm::value_ptr(proj * view));
+}
+
+void Camera::HandleInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        position += speed * forward;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        position += speed * -glm::normalize(glm::cross(forward, up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        position += speed * -forward;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        position += speed * glm::normalize(glm::cross(forward, up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        position += speed * up;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+    {
+        position += speed * -up;
+    }
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+        if (firstClick)
+        {
+            glfwSetCursorPos(window, width / 2.0f, height / 2.0f);
+            firstClick = false;
+        }
+
+        double mouseX;
+        double mouseY;
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+
+        float rotX = sensitivity * (float)(mouseY - height / 2.0f) / height;
+        float rotY = sensitivity * (float)(mouseX - width / 2.0f) / width;
+
+        glm::vec3 newOrientation = glm::rotate(forward, glm::radians(-rotX),
+            glm::normalize(glm::cross(forward, up)));
+
+        if (abs(glm::angle(newOrientation, up) - glm::radians(90.0f))
+            <= glm::radians(85.0f))
+        {
+            forward = newOrientation;
+        }
+
+        forward = glm::rotate(forward, glm::radians(-rotY), up);
+        glfwSetCursorPos(window, width / 2.0f, height / 2.0f);
+    }
+    else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+    {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        firstClick = true;
+    }
+}
---
+++ StereoscopicRenderer/Camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include "Shader.h"
+
+class Camera
+{
+public:
+    Camera(int width, int height, glm::vec3 position);
+
+    void Matrix(float fovDeg, float nearPlane, float farPlane, Shader& shader,
+        const char* uniform);
+
+    void HandleInput(GLFWwindow* window);
+
+private:
+    int width;
+    int height;
+    glm::vec3 position;
+    glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    float speed = 0.004f;
+    float sensitivity = 100.0f;
+    bool firstClick = true;
+};
