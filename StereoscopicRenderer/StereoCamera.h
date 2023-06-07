2f55dccbeacda69bfb848f9cf2b2cc7db6246d46
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-06-06 18:43:44 -0700

    Update camera direction

--- StereoscopicRenderer/StereoCamera.h
+++ StereoscopicRenderer/StereoCamera.h
@@ -20,7 +20,7 @@ private:
     int width;
     int height;
     glm::vec3 position;
-    glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 forward = glm::vec3(0.0f, 0.0f, 1.0f);
     glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
     float speed = 0.05f;
     float sensitivity = 100.0f;
